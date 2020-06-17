293b40773fd6de8c5a6dc9d5ff709af3ed411fb7
ygl <ygl@x230>
2020-06-17 11:00:13 +0000

    u

--- t5.py
+++ t5.py
@@ -13,7 +13,8 @@ class MainWindow(qtw.QMainWindow):
         self.text_edit.textChanged.connect(lambda: char_count_label.setText("chars: {0}".format(len(self.text_edit.toPlainText()))))
         self.statusBar().addPermanentWidget(char_count_label)
         file_menu = self.menuBar().addMenu('文件')
-        file_menu.addAction('打开')
+        file_menu.addAction('打开', self.open_file)
+        file_menu.addAction('设置字体', self.set_font)
         file_menu.addAction('退出', self.close)
 
         toolbar1 = self.addToolBar('文件')
@@ -22,6 +23,20 @@ class MainWindow(qtw.QMainWindow):
         dock = qtw.QDockWidget("Replace")
         self.addDockWidget(qtc.Qt.LeftDockWidgetArea, dock)
         self.show()
+
+    def open_file(self):
+        file_name = qtw.QFileDialog.getOpenFileName(self, initialFilter='*.py')
+        print(file_name)
+
+    def show_question(self):
+        result = qtw.QMessageBox.question(self, "测试", "是否打开")
+        print(result)
+
+    def set_font(self):
+        current = self.text_edit.currentFont()
+        font, accepted = qtw.QFontDialog.getFont(current, self)
+        if accepted:
+            self.text_edit.setCurrentFont(font)
 
 
 if __name__ == "__main__":
